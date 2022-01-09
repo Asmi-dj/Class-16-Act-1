e20361e44a19d0dd98e57765300d14db56b005f0
Asmi-dj <96016808+Asmi-dj@users.noreply.github.com>
2022-01-09 11:30:11 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class Box
+  {
+    constructor()
+    {
+      this.x = 100;
+      this.y = 200
+      this.w = 50;
+      this.h = 50;
+    }
+    
+    show()
+    {
+      rect(this.x,this.y,this.w,this.h)
+    }
+    
+    //write function to set the width of the box
+     set_width(val)
+     {
+       this.w = val 
+     }
+
+     set_colour(cval)
+     {
+       fill(cval);
+     }
+  }
+
+  
